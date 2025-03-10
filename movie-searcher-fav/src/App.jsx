2a7125515674adaf97e0bf5db294e11802397ee0
Gagandeep singh <gaganarora987123@gmail.com>
2025-03-10 11:46:54 +0530

    basic template and Added Movie card

---
+++ movie-searcher-fav/src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+import MovieCard from "./components/MovieCard";
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ movie-searcher-fav/src/components/MovieCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const MovieCard = ({ movie }) => {
+  function handlefav() {
+    alert("clicked");
+  }
+  return (
+    <div className="movie-card">
+      <div className="movie-img">
+        <img src={movie.url} alt={movie.title} />
+        <div className="movie-overlay">
+          <button className="fav-btn" onClick={handlefav()}>
+            Fav
+          </button>
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
